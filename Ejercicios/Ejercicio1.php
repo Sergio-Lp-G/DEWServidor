57038218c85052123d3c5ae62091ebeaae574481
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-09-23 17:54:57 +0100

    carpeta ejercicios

---
+++ Ejercicios/Ejercicio1.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 144;
+$y = 999;
+echo "$x <br>";
+echo "$y <br>";
+
+$suma = $x + $y;
+echo "$suma <br>";
+
+$resta = $x - $y;
+echo "$resta <br>";
+
+$division = $x / $y;
+echo "$division <br>";
+
+$producto = $x * $y;
+echo "$producto <br>";
