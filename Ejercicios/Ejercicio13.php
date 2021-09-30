0bc17c50bf200ffec395875ee22188f4f51f4487
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-09-30 15:34:13 +0100

    com

--- Ejercicios/Ejercicio13.php
+++ Ejercicios/Ejercicio13.php
@@ -1,13 +1,29 @@
 <?php
 $a = 5;
-$b = 100;
-$c = -3;
+$b = 0;
+$c = 3;
 
 echo segundoGrado($a, $b, $c);
 
 function segundoGrado($a, $b, $c)
 {
-    $solpos = (-$b + sqrt(pow($b, 2) - (4 * $a * $c))) / (2 * $a);
-    $solneg = (-$b - sqrt(pow($b, 2) - (4 * $a * $c))) / (2 * $a);
-    return $solpos . "<br>" . $solneg;
+    $discriminante=(pow($b,2))-(4*$a*$c);
+    if($discriminante<0){
+        $solpos = (-$b + sqrt(pow($b, 2) - (4 * $a * $c))) / (2 * $a);
+        $solneg = (-$b - sqrt(pow($b, 2) - (4 * $a * $c))) / (2 * $a);
+        $resultados = array (
+            "x1" => $solpos,
+            "x2" => $solneg);
+        return $resultados;
+    }
+    else{
+        $bolean=FALSE;
+        $resultados = array (
+            "x1" => $bolean);
+        return $resultados;
+        
+    }
+    foreach (segundoGrado($a, $b, $c) as $posicion => $nombre) {
+        echo  $posicion.": " . $nombre." <br>";
+    }
 }
