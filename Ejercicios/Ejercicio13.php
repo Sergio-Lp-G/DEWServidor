8533ba0f6f2750a0d419daf130d0937bdd2f82d6
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-09-30 17:21:00 +0100

    com

--- Ejercicios/Ejercicio13.php
+++ Ejercicios/Ejercicio13.php
@@ -1,37 +1,10 @@
 <?php
-$a = 5;
-$b = 10;
-$c = 3;
 
-echo $a." <br>". $b." <br>". $c." <br>";
+$num=5;
+$num1=1;
+$num2=1;
+//require __DIR__ . '/mates.php';
+require_once('Math.php');
+ 
+echo "Los resultados son :" , print_r(SegundoGrado($num,$num1,$num2));
 
-function discriminante ($a, $b, $c){
-    $discriminante=(pow($b,2))-(4*$a*$c);
-    //echo $discriminante." <br>";
-    return $discriminante;
-}
-discriminante ($a, $b, $c);
-
-function segundoGrado($a, $b, $c)
-{
-    if(discriminante($a, $b, $c)>0){
-        $solpos = (-$b + sqrt(pow($b, 2) - (4 * $a * $c))) / (2 * $a);
-        $solneg = (-$b - sqrt(pow($b, 2) - (4 * $a * $c))) / (2 * $a);
-        //echo $solpos." <br>".$solneg;
-        $resultados = array (
-            "x1" => $solpos,
-            "x2" => $solneg);
-        return $resultados;
-    }
-    else{
-        $nosol="FALSE";
-        //echo $nosol." <br>";
-        $resultados = array (
-            "Resultado" => $nosol);
-        return $resultados;
-        
-    }
-}
-foreach (segundoGrado($a, $b, $c) as $resultados => $nombre) {
-    echo  $resultados.": " . $nombre." <br>";
-}
